56e5b988d370d0251c86a170a48d6ab0bd0ddf41
Mark McTamney <30511006+markmctamney@users.noreply.github.com>
2020-10-10 09:51:50 -0400

    Change default repo naming to add @[owner]

--- app/src/ui/clone-repository/clone-repository.tsx
+++ app/src/ui/clone-repository/clone-repository.tsx
@@ -545,6 +545,10 @@ export class CloneRepository extends React.Component<
       newPath = tabState.path
     }
 
+    if (parsed?.owner) {
+      newPath = `${newPath}@${parsed.owner}`
+    }
+
     this.setSelectedTabState(
       {
         url,
